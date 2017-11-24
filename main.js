6ddc0bb8f07ea5856bbbd9549982036261f45fa2
rajak <rajak@adobe.com>
2017-11-24 17:47:47 +0530

    Box controllers added

--- main.js
+++ main.js
@@ -13,6 +13,11 @@
  * limitations under the License.
  */
 
+/*
+* namespace for box api specific controls
+*/
+var box = {};
+
 'use strict';
 
 if (!PDFJS.PDFViewer || !PDFJS.getDocument) {
@@ -22,17 +27,37 @@ if (!PDFJS.PDFViewer || !PDFJS.getDocument) {
 
 // The workerSrc property shall be specified.
 //
-PDFJS.workerSrc = 'node_modules/pdfjs-dist/build/pdf.worker.js';
+PDFJS.workerSrc = 'imports/pdfjs-dist/pdf.worker.js';
 
 // Some PDFs need external cmaps.
 //
 // PDFJS.cMapUrl = '../../node_modules/pdfjs-dist/cmaps/';
 // PDFJS.cMapPacked = true;
 
-var DEFAULT_URL = 'https://www.dropbox.com/s/er860hrjujostwo/Dropbox%20Getting%20Started.pdf?dl=1';
+box.moduleUrl = 'https://www.dropbox.com/s/er860hrjujostwo/Dropbox%20Getting%20Started.pdf?dl=1';
+box.initialized = false;
 var SEARCH_FOR = ''; // try 'Mozilla';
 
 var container = document.getElementById('viewerContainer');
+
+// Read query params
+var moduleContentUrlParams = window.location.search.substring(1);
+var paramsArray = moduleContentUrlParams.split("&");
+for (var i=0; i<paramsArray.length; i++) {
+  var sParameterName = paramsArray[i].split("=");
+  if (sParameterName[0] === "moduleUrl") {
+      box.moduleUrl = sParameterName[1] ? sParameterName[1].split("?")[0] : "";
+  }
+  if (sParameterName[0] === "parentApp") {
+      box.callingapp = sParameterName[1];
+      if (box.callingapp === "contentPlayer") {
+      /* @ifndef  DEVICE_TARGET*/
+          document.domain = "adobe.com";
+      /* @endif */
+      }
+  }
+}
+
 
 // (Optionally) enable hyperlinks within PDF files.
 var pdfLinkService = new PDFJS.PDFLinkService();
@@ -49,20 +74,374 @@ var pdfFindController = new PDFJS.PDFFindController({
 });
 pdfViewer.setFindController(pdfFindController);
 
+// Loading document.
+PDFJS.getDocument(box.moduleUrl).then(function (pdfDocument) {
+  // Document loaded, specifying document for the viewer and
+  // the (optional) linkService.
+  if (box.initialized) {
+    return;
+  }
+  pdfViewer.setDocument(pdfDocument);
+
+  pdfLinkService.setDocument(pdfDocument, null);
+  box.ACAPInterface.setNumPagesFromEvent(pdfDocument);
+});
+
+
 container.addEventListener('pagesinit', function () {
   // We can use pdfViewer now, e.g. let's change default scale.
+  if (box.initialized) {
+    return;
+  }
   pdfViewer.currentScaleValue = 'page-width';
 
   if (SEARCH_FOR) { // We can try search for things
     pdfFindController.executeCommand('find', {query: SEARCH_FOR});
   }
+  box.onBoxDocumentViewable();
+  box.initialized = true;
 });
 
-// Loading document.
-PDFJS.getDocument(DEFAULT_URL).then(function (pdfDocument) {
-  // Document loaded, specifying document for the viewer and
-  // the (optional) linkService.
-  pdfViewer.setDocument(pdfDocument);
+box.onBoxDocumentViewable = function() {
+  box.ACAPInterface.updateCurrentPageFromEvent(pdfViewer);
+  container.addEventListener('pagechange', box.ACAPInterface.updateCurrentPageFromEvent);
+  // container.addEventListener("focus", box.ACAPInterface.updateCurrentPageFromEvent);
+  // pdfViewer.eventBus.on('pageChange', box.ACAPInterface.updateCurrentPageFromEvent);
+  var evt = document.createEvent("Event");
+  evt.initEvent("moduleReadyEvent", true, true);
+  evt.Data = box.ACAPInterface;
+  box.viewer = pdfViewer;
 
-  pdfLinkService.setDocument(pdfDocument, null);
+  /* after this call reporting options are set*/
+  if (window.parent) {
+    window.parent.dispatchEvent(evt);
+  }
+
+
+  box.updatePagesViewed(pdfViewer);
+  box.updateCompletionIfCriteriaMet(pdfViewer);
+  box.updateSuccessIfCriteriaMet(pdfViewer);
+  box.triggerStartedEvent(pdfViewer);
+
+  box.getDataFromAcapPlayer();
+
+}
+
+
+/**************************************************************************/
+
+
+/********************************** tracking data**************************/
+box.viewedPages = {};
+box.numPagesViewed = 0;
+box.updatePagesViewed = function(event) {
+    var page = event.currentPageNumber ? event.currentPageNumber : event.pageNumber;
+    if(!box.viewedPages[page]) {
+      box.viewedPages[page] = true;
+      box.numPagesViewed++;
+    }
+    // box.viewer.currentPageNumber = page;
+}
+
+
+/*********************************tracking data ***************************/
+
+/* Utlity functions and classes for reporting and analytics*/
+
+box.ResumePlaybackCallbackFn = function(resumeDataPromise) {
+    resumeDataPromise.then( function(previousSessionData) {
+        console.log(previousSessionData);
+        var resumelocation = parseInt(previousSessionData, 10);
+        console.log("resuming from" + resumelocation + "location");
+        // box.viewer.currentPageNumber = resumelocation;
+        box.triggerSlideResumeDone();
+    }, function() {
+        console.log("failed to obtain resume data from server");
+    });
+};
+
+box.SetViewedPagesFromServer = function(resumeDataPromise) {
+    resumeDataPromise.then( function(previousSessionData) {
+        console.log(previousSessionData);
+        var previousSessionDataObject = JSON.parse(previousSessionData);
+        if (previousSessionDataObject.pagesViewedHash) {
+            box.viewedPages = previousSessionDataObject.pagesViewedHash;
+        }
+        if (previousSessionDataObject.numPagesViewed) {
+            box.numPagesViewed = previousSessionDataObject.numPagesViewed;
+        }
+    }, function() {
+        console.log("failed to obtain resume data from server");
+    });
+}
+
+box.getDataFromAcapPlayer = function(){
+    var eventObject = new Object();
+    eventObject.Name = "CAPI_GET";
+    eventObject.Data = {};
+    eventObject.Data.ask = "resumeData";
+    eventObject.Data.callback = box.ResumePlaybackCallbackFn;
+    box.EventEmitter.customEventTrigger(eventObject);
+
+    eventObject.Name = "CAPI_GET";
+    eventObject.Data = {};
+    eventObject.Data.ask = "viewedPages";
+    eventObject.Data.callback = box.SetViewedPagesFromServer;
+    box.EventEmitter.customEventTrigger(eventObject);
+};
+
+box.setDataToAcapPlayer = function(currentSessionData){
+    var eventObject = new Object();
+    eventObject.Name = "CAPI_SET_RESUMEDATA";
+    eventObject.Data = {};
+    eventObject.Data.ResumeData = currentSessionData;
+    box.EventEmitter.customEventTrigger(eventObject);
+
+    eventObject.Name = "CAPI_SET_MODULEDATA";
+    eventObject.Data = {};
+    eventObject.Data.pagesViewedHash = box.viewedPages;
+    eventObject.Data.numPagesViewed = box.numPagesViewed;
+    box.EventEmitter.customEventTrigger(eventObject);
+};
+
+box.updateCompletionIfCriteriaMet = function (event) {
+    switch(event.type) {
+        case "pagefocus":
+        case "ready":
+        try {
+            if(box.completionCriteria.criteria === "onLaunch"){
+                box.triggerCompletionEvent();
+            }
+            if(box.completionCriteria.criteria === "onPercentageView") {
+                if(box.completionCriteria.viewPercent && box.numPagesViewed) {
+                    if(((box.numPagesViewed/event.data.numPages) * 100) >= box.completionCriteria.viewPercent) {
+                        box.triggerCompletionEvent();
+                    }
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        break;
+
+
+    }
+}
+
+box.updateSuccessIfCriteriaMet = function (event) {
+    switch(event.type) {
+        case "pagefocus":
+        case "ready":
+            // as box viewer can be used in job aids as well where there is cc/sc criteria ensuring slide exit event is still called
+            try {
+                if(box.successCriteria.criteria === "onLaunch"){
+                    box.triggerPassedEvent();
+                }
+                if(box.successCriteria.criteria === "onPercentageView") {
+                    if(box.successCriteria.viewPercent && box.numPagesViewed) {
+                        if(((box.numPagesViewed/event.data.numPages) * 100) >= box.successCriteria.viewPercent) {
+                            box.triggerPassedEvent();
+                        }
+                    }
+                }
+                box.triggerSlideExitEvent(event);
+            } catch (error)  {
+                box.triggerSlideExitEvent(event);
+            }
+            break;
+    }
+}
+
+
+/*
+* ACAPEventEmitterClass - event manager for Box.
+*/
+
+
+box.ACAPEventEmitterClass = function() {
+        this.callbackFns = new Array();
+        this.previousSuspendData = 0;
+
+    };
+
+box.ACAPEventEmitterClass.prototype.addEventListener = function(fn) {
+    var index = this.callbackFns.indexOf(fn);
+    if(index < 0)
+        this.callbackFns.push(fn);
+}
+
+box.ACAPEventEmitterClass.prototype.removeEventListener = function(fn) {
+    var index = this.callbackFns.indexOf(fn);
+    if( index > -1){
+        this.callbackFns.splice(index, 1);
+    }
+
+}
+
+box.ACAPEventEmitterClass.prototype.trigger = function(event) {
+    switch(event.type) {
+
+        // map pageFocus to CAPI_SLIDEEXIT
+        // add an additional Name property to the event object
+        case "pagechange":
+            event.Name = "CAPI_SLIDEEXIT";
+            event.data = {};
+            event.data.page = event.pageNumber;
+            event.data.numPages = box.ACAPInterface.getDurationInPages();
+            try {
+                box.updatePagesViewed(event);
+                box.updateSuccessIfCriteriaMet(event);
+                box.updateCompletionIfCriteriaMet(event);
+                box.setDataToAcapPlayer(event.data.page);
+            } catch (e) {
+                for (var i = 0; i < this.callbackFns.length; i++) {
+                    this.callbackFns[i](event);
+                }
+            }
+            break;
+        case "pageerror":
+            event.Name = "CAPI_ERROR";
+            break;
+        case "zoom":
+            event.Name = "CAPI_ZOOM";
+            break;
+    }
+
+    for (var i = 0; i < this.callbackFns.length; i++) {
+        this.callbackFns[i](event);
+    }
+}
+
+box.ACAPEventEmitterClass.prototype.customEventTrigger = function(event) {
+    for (var i = 0; i < this.callbackFns.length; i++) {
+        this.callbackFns[i](event);
+    }
+}
+
+
+box.triggerStartedEvent = function(event) {
+    var eventObject = new Object();
+    eventObject.Name = "CAPI_STARTED";
+    box.EventEmitter = box.ACAPInterface.getEventEmitter();
+    box.EventEmitter.customEventTrigger(eventObject);
+}
+
+box.triggerCompletionEvent = function(){
+    var eventObject = new Object();
+    eventObject.Name = "CAPI_COMPLETED";
+    box.EventEmitter.customEventTrigger(eventObject);
+};
+
+box.triggerPassedEvent = function(){
+    var eventObject = new Object();
+    eventObject.Name = "CAPI_PASSED";
+    box.EventEmitter.customEventTrigger(eventObject);
+};
+
+box.triggerSlideExitEvent = function(event){
+    event.Name = "CAPI_SLIDEEXIT";
+    box.EventEmitter.customEventTrigger(event);
+};
+
+box.triggerSlideResumeDone = function(){
+    var event = new Object();
+    event.Name = "CAPI_SLIDERESUME";
+    box.EventEmitter.customEventTrigger(event);
+};
+
+/*
+* ACAPInterface - interface object provided to ACAP player to control the playback of the box.
+*/
+
+box.ACAPInterface = new (function(){
+
+    /*private properties */
+
+    var currentPage;
+    var numPages;
+
+    /* public getters and setters*/
+
+    this.getCurrentPage = function () {
+        return currentPage;
+    },
+
+    this.getDurationInPages = function () {
+        return numPages;
+    },
+
+    this.setNumPagesFromEvent = function(event) {
+        numPages = event.numPages;
+    },
+
+    this.updateCurrentPageFromEvent = function (event) {
+        currentPage = event.currentPageNumber ? event.currentPageNumber: event.pageNumber;
+    }
+
+    /*public properties */
+    this.seek =  function(pageNo) {
+        if (pdfViewer !== null) {
+            pdfViewer.currentPageNumber = pageNo;
+        }
+    },
+
+    this.getEventEmitter = function(){
+        if (!box.EventEmitter) {
+            box.EventEmitter = new box.ACAPEventEmitterClass();
+            container.addEventListener("pagechange", $.proxy(box.EventEmitter.trigger, box.EventEmitter));
+            container.addEventListener("zoom", $.proxy(box.EventEmitter.trigger, box.EventEmitter));
+        }
+        return box.EventEmitter;
+    }
+
+    this.zoomIn = function() {
+        // box.viewer.zoom(Crocodoc.ZOOM_IN);
+        pdfViewer.currentScale = pdfViewer.currentScale + 0.1;
+    },
+
+    this.zoomOut = function() {
+        // box.viewer.zoom(Crocodoc.ZOOM_OUT);
+        if (pdfViewer.currentScale < 0.25) {
+          return;
+        }
+        pdfViewer.currentScale = pdfViewer.currentScale - 0.1;
+    },
+
+    this.scrollToNext = function() {
+        if (currentPage < numPages) {
+          currentPage++;
+          pdfViewer.currentPageNumber = currentPage;
+        }
+    },
+
+    this.scrollToPrev = function() {
+        if (currentPage > 1) {
+          currentPage--;
+          pdfViewer.currentPageNumber = currentPage;
+        }
+    },
+
+    this.scrollTo = function(pageNo) {
+        pdfViewer.currentPageNumber = pageNo;
+    },
+
+    this.setReportingOptions = function (successCriteria, completionCriteria) {
+        box.completionCriteria = {};
+        box.successCriteria = {};
+        if(completionCriteria.CClaunchContent) {
+            box.completionCriteria.criteria = "onLaunch";
+        } else if(completionCriteria.CCslideViewPercent !== 0) {
+            box.completionCriteria.criteria = "onPercentageView";
+            box.completionCriteria.viewPercent = completionCriteria.CCslideViewPercent;
+        }
+
+
+        if(successCriteria.SClaunchContent) {
+            box.successCriteria.criteria = "onLaunch";
+        } else if(successCriteria.SCslideViewPercent !== 0) {
+            box.successCriteria.criteria = "onPercentageView";
+            box.successCriteria.viewPercent = successCriteria.SCslideViewPercent;
+        }
+    }
+
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
+const htmlPlugin = require('html-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
 var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
   context: __dirname,
   entry: {
-    'main': './main.js',
-    'pdf.worker': './node_modules/pdfjs-dist/build/pdf.worker.entry'
+    'main': './main.js'
   },
   output: {
     path: path.join(__dirname, 'dist/'),
-    publicPath: './dist/',
-    filename: '[name].bundle.js'
+    publicPath: './',
+    filename: '[name].js'
+  },
+
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '$': 'jQuery'
+    }
   },
 
   plugins: [
+    new htmlPlugin({
+        title: 'Embeddable Player',
+        filename: 'index.html',
+        template: 'index.html'
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         screw_ie8: true,
         warnings: false
       }
-    })
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new copyWebpackPlugin([
+      {from: 'lib'}
+    ])
   ]
 };
